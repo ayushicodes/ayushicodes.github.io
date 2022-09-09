3c9a249d46ef390a7bf91b551cec697c6ade2fca
Ayushi Rai <ayushicodes@gmail.com>
2022-09-09 15:49:13 +0530

    Css updated and link added

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+
+
 let about = document.querySelector('.about')
 let btn = document.querySelectorAll('.tab-btn')
 let content = document.querySelectorAll('.content')
@@ -60,115 +64,134 @@ about.addEventListener('click', function (e) {
     }
 })
 
-// projects section
+
 
 const menu = [
+
     {
         id: 1,
-        title: "Emoji Translater",
-        category: "react",
-        img: "images/emojitranslater.png",
-        desc: `I am a emoji translater `,
+        title: "breakout",
+        category: "javascript",
+        // price: 13.99,
+        img: "images/breakout.png",
+        desc: `In Breakout, a layer of bricks lines the top third of the screen and the goal is to destroy them all by repeatedly bouncing a ball off a paddle into them. `,
+        link: `https://breakout00.netlify.app`
     },
     {
         id: 2,
-        title: "Memory Game",
-        category: "javascript",
-        price: 20.99,
-        img: "images/memorygame.png",
-        desc: `I am a memory game `,
+        title: "emojitranslater",
+        category: "react",
+        // price: 6.99,
+        img: "images/emojitranslater.png",
+        desc: `
+        EmojiTranslater translates emojis to texts in english language`,
+        link: `https://emoji-translater.netlify.app`
     },
     {
         id: 3,
-        title: "Minion Speak",
+        title: "Todo",
         category: "javascript",
-        img: "images/minionspeak.png",
-        desc: `I am a minion speak `,
+        // price: 20.99,
+        img: "images/grocerybud.png",
+        desc: `You can create a list and can edit and delete the individual item or whole list. `,
+        link: `https://to-do-app00.netlify.app`
     },
     {
         id: 4,
-        title: "Wac a mole",
+        title: "memory game",
         category: "javascript",
-        img: "images/wacamole.png",
-        desc: `I am  wac a mole`,
+        price: 22.99,
+        img: "images/memorygame.png",
+        desc: `It is a simple card game of finding two similar image. It imporves visual recognition `,
+        link: `https://memory-game00.netlify.app`
     },
     {
         id: 5,
-        title: "BreakOUT",
+        title: "minionspeak",
         category: "javascript",
-        img: "images/breakout.png",
-        desc: `I am  breakout`,
+        price: 18.99,
+        img: "images/minionspeak.png",
+        desc: `It translate english language to banana language using an API.`,
+        link: `https://minion-speak-00.netlify.app`
+    },
+    {
+        id: 6,
+        title: "whac a mole",
+        category: "javascript",
+        price: 8.99,
+        img: "images/wacamole.png",
+        desc: `Moles appear randomly in any square. Points are scored by whacking each mole as it appears.`,
+        link: `https://wac-a-mole00.netlify.app`
     },
 
 ];
-// get parent element
-const sectionCenter = document.querySelector(".Projectsection-center");
-const btnContainer = document.querySelector(".projectbtn-container");
-// display all items when page loads
-window.addEventListener("DOMContentLoaded", function () {
-    diplayMenuItems(menu);
-    displayMenuButtons();
-});
+let sectionCenter = document.querySelector('.projectSection-center')
+let btnCointainer = document.querySelector(".projectBtn-container")
 
-function diplayMenuItems(menuItems) {
-    let displayMenu = menuItems.map(function (item) {
-        // console.log(item);
 
-        return `<article class="project-item">
-            <img src=${item.img} alt=${item.title} class="photo" />
-            <div class="project-info">
-              <header>
-                <h4>${item.title}</h4>
-              </header>
-              <p class="project-text
-              ">
-                ${item.desc}
-              </p>
-            </div>
-          </article>`;
-    });
-    displayMenu = displayMenu.join("");
-    // console.log(displayMenu);
 
-    sectionCenter.innerHTML = displayMenu;
-}
-function displayMenuButtons() {
-    const categories = menu.reduce(
-        function (values, item) {
-            if (!values.includes(item.category)) {
-                values.push(item.category);
-            }
-            return values;
-        },
-        ["all"]
-    );
-    const categoryBtns = categories
-        .map(function (category) {
-            return `<button type="button" class="filter-btn" data-id=${category}>
-            ${category}
-          </button>`;
-        })
-        .join("");
+window.addEventListener('DOMContentLoaded', function () {
+    displayMenu(menu)
+    displayMenuBtns()
+})
 
-    btnContainer.innerHTML = categoryBtns;
-    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
-    console.log(filterBtns);
 
-    filterBtns.forEach(function (btn) {
-        btn.addEventListener("click", function (e) {
-            // console.log(e.currentTarget.dataset);
-            const category = e.currentTarget.dataset.id;
-            const menuCategory = menu.filter(function (menuItem) {
-                // console.log(menuItem.category);
-                if (menuItem.category === category) {
-                    return menuItem;
+
+function displayMenuBtns() {
+    const categories = menu.reduce(function (value, currValue) {
+        if (!value.includes(currValue.category)) {
+            value.push(currValue.category)
+        }
+        return value
+    }, ["all"]
+    )
+    const categoryBtn = categories.map(function (category) {
+        return ` <button type="button" class="filter-btn" data-id=${category}>${category}</button>`
+    }).join('')
+    btnCointainer.innerHTML = categoryBtn
+
+    let filterbtn = document.querySelectorAll(".filter-btn")
+
+    filterbtn.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            let category = e.currentTarget.dataset.id
+            // console.log(category)
+            let displayCategory = menu.filter(function (item) {
+                if (item.category === category) {
+                    return item
                 }
-            });
-            if (category === "all") {
-                diplayMenuItems(menu);
-            } else {
-                diplayMenuItems(menuCategory);
+            })
+            if (category === 'all') {
+                return displayMenu(menu)
             }
-        });
-    });
+            else {
+                return displayMenu(displayCategory)
+            }
+        })
+    })
 }
+
+function displayMenu(menuItems) {
+    let display = menuItems.map(function (item) {
+
+        return `  <article class="project-item">
+        <img src=${item.img} 
+            alt=${item.title} class="photo" />
+        <div class="project-info">
+            <header>
+                <h4><a target="_blank" href=${item.link}>${item.title}</a></h4>
+               
+            </header>
+            <p class="project-text">
+               ${item.desc}</br>
+               
+            </p>
+        </div>
+    </article>`
+    }).join('')
+    sectionCenter.innerHTML = display
+}
+
+
+
+
